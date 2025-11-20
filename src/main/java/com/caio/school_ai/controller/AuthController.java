31de3a8ecfec1656d1d142caee731b661b338f51
Caio <caiodesouza.cds@gmail.com>
2025-11-20 15:16:08 -0300

    Realizando login

--- src/main/java/com/caio/school_ai/controller/AuthController.java
+++ src/main/java/com/caio/school_ai/controller/AuthController.java
@@ -1,8 +1,13 @@
 package com.caio.school_ai.controller;
 
+import com.caio.school_ai.model.entity.Usuario;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("/auth")
@@ -16,6 +21,19 @@ public class AuthController {
     @GetMapping("/login")
     public String login(){
         return "auth/login";
+    }
+
+    @GetMapping("/cadastro")
+    public String cadastro(){
+        return "auth/cadastro";
+    }
+
+    @RequestMapping(value = "/cadastro", method = RequestMethod.POST)
+    public String cadastro(@Valid Usuario usuario, BindingResult resultado){
+        if(resultado.hasErrors()){
+            return "redirect:/cadastro";
+        }
+        return "";
     }
 
 }
