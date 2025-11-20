29f23817104a78e212656636d3d79fc5471bf811
Caio <caiodesouza.cds@gmail.com>
2025-11-19 23:43:35 -0300

    Front tela inicial feito

--- src/main/java/com/caio/school_ai/controller/InicialController.java
+++ src/main/java/com/caio/school_ai/controller/InicialController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/")
-public class IniciaController {
+public class InicialController {
 
     @GetMapping
     public String inicial(){
